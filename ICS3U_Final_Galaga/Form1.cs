5e69e78bdcf22131399774721ff6178064f43adf
declano777 <declano2009@icloud.com>
2026-01-06 10:24:13 -0500

    Add project files.

---
+++ ICS3U_Final_Galaga/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICS3U_Final_Galaga
+{
+    public partial class Form1 : Form
+    {
+        // PLAYER
+        int playerX = 360;
+        int playerY = 730;
+        int playerWidth = 40;
+        int playerHeight = 20;
+        int playerSpeed = 3;
+
+        bool moveLeft = false;
+        bool moveRight = false;
+
+        // PLAYER BULLET
+        int bulletX;
+        int bulletY;
+        int bulletWidth = 4;
+        int bulletHeight = 10;
+        int bulletSpeed = 6;
+        bool bulletActive = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                moveLeft = true;
+
+            if (e.KeyCode == Keys.D)
+                moveRight = true;
+
+            if (e.KeyCode == Keys.Space && !bulletActive)
+            {
+                bulletActive = true;
+                bulletX = playerX + playerWidth / 2 - bulletWidth / 2;
+                bulletY = playerY;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                moveLeft = false;
+
+            if (e.KeyCode == Keys.D)
+                moveRight = false;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // DRAW PLAYER
+            e.Graphics.FillRectangle(
+                Brushes.Cyan,
+                playerX,
+                playerY,
+                playerWidth,
+                playerHeight
+            );
+
+            // DRAW BULLET
+            if (bulletActive)
+            {
+                e.Graphics.FillRectangle(
+                    Brushes.Yellow,
+                    bulletX,
+                    bulletY,
+                    bulletWidth,
+                    bulletHeight
+                );
+
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // PLAYER MOVEMENT
+            if (moveLeft && playerX > 0)
+                playerX -= playerSpeed;
+
+            if (moveRight && playerX + playerWidth < ClientSize.Width)
+                playerX += playerSpeed;
+
+            // PLAYER BULLET MOVEMENT
+            if (bulletActive)
+            {
+                bulletY -= bulletSpeed;
+
+                if (bulletY < 0)
+                    bulletActive = false;
+            }
+            UpdatePlayer();
+            this.Invalidate();
+        }
+        void UpdatePlayer()
+        {
+            if (moveLeft && playerX > 0)
+                playerX -= playerSpeed;
+
+            if (moveRight && playerX + playerWidth < ClientSize.Width)
+                playerX += playerSpeed;
+
+            if (bulletActive)
+            {
+                bulletY -= bulletSpeed;
+                if (bulletY < 0)
+                    bulletActive = false;
+            }
+        }
+    }
+}
---
+++ ICS3U_Final_Galaga/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ICS3U_Final_Galaga")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICS3U_Final_Galaga")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4befe80-10eb-4fb8-b8a4-622b12d4eb59")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
